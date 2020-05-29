3fc44b3700c8c446e67d775ecdad524a65a0bd99
抖音：刀某人 <859346593@qq.com>
2020-05-29 19:52:23 +0800

    Add files via upload

---
+++ tv_52meme_top/static/js/tj.js
@@ -0,0 +1,26 @@
+<script type="text/javascript" src="https://v1.cnzz.com/z_stat.php?id=1278731308&web_id=1278731308"></script>
+<br>
+<!--百度自动推送代码区域开始-->
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+<!--百度搜索引擎自动推送代码区域结束-->
+<!--360搜索引擎自动推送代码区域开始-->
+<script>
+(function(){
+var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
+</script>
+<!--360搜索引擎自动推送代码区域结束-->
